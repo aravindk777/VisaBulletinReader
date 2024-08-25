a861b2ea3c0d46eaca8780d387f9ced1eee44e82
Aravindk777 <40478917+aravindk777@users.noreply.github.com>
2024-08-24 23:55:10 -0400

    Update visabulletinreader.py
    Included bulletin links and section reading

--- src/visabulletinreader.py
+++ src/visabulletinreader.py
@@ -1,17 +1,40 @@
 import configparser
 from datetime import datetime
 from bs4 import BeautifulSoup
+import requests
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 def Main():
     currentYear = datetime.now().year
-    bulletinUrlBase = config["SEARCH"]["BASE_URL"]
+    currentMonth = datetime.now().strftime("%B")
+    print(f"Current Year: {currentYear} - Current Month: {currentMonth} | Required format: {currentMonth}-{currentYear}")
+    bulletinUrlBase = config["SEARCH"]["BASE_URL_DOMAIN"] + config["SEARCH"]["BASE_URL"]
     ProcessBulletinUrl(bulletinUrlBase)
 
+def GetBulletinMonthUrl(page: BeautifulSoup, searchText: str) -> str:
+    h2_tag = page.select_one(f'h2:-soup-contains("{searchText}")')
+    if h2_tag:
+        hyperlink = h2_tag.find_next('a')
+        if hyperlink is not None:
+            print("Adjacent Hyperlink found:", hyperlink['href'], "hyperlink text:", hyperlink.text)
+            return hyperlink['href']
+
 def ProcessBulletinUrl(baseUrl: str):
-    primaryPage = BeautifulSoup()
+    pageHtml = requests.get(baseUrl, verify=False)
+    primaryPage = BeautifulSoup(pageHtml.text, 'html.parser')
+    hrefToBulletin = ""
+    if "Upcoming Visa Bulletin" in primaryPage.text:
+        print("Found 'Upcoming Visa Bulletin'")
+        hrefToBulletin = GetBulletinMonthUrl(primaryPage, "Upcoming Visa Bulletin")
+    
+    if hrefToBulletin is None or hrefToBulletin == "":
+        print("Upcoming Visa Bulletin not found")
+        hrefToBulletin = GetBulletinMonthUrl(primaryPage, "Current Visa Bulletin")
+    
+    hrefToBulletin = config["SEARCH"]["BASE_URL_DOMAIN"] + hrefToBulletin
+    print("Bulletin URL:", hrefToBulletin) 
 
 if __name__ == '__main__':
     Main()
