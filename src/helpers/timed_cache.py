e15a9e01d803ff3f7c20d842b3891a0180cb2524
Aravindk777 <yrsk.aravind@gmail.com>
2025-09-21 21:24:56 -0400

    refactor documentation for timed_lru_cache decorator to improve clarity and completeness

--- src/helpers/timed_cache.py
+++ src/helpers/timed_cache.py
@@ -6,13 +6,13 @@ The cache is cleared after a specified number of seconds.
 from datetime import timedelta, datetime, timezone
 from functools import lru_cache, wraps
 
-"""
-Function to create a timed LRU cache decorator.
-:param seconds: The number of seconds before the cache expires.
-:param maxsize: The maximum size of the LRU cache.
-:return: The decorated function with caching.
-"""
 def timed_lru_cache(seconds: int, maxsize: int = None):
+    """
+    Function to create a timed LRU cache decorator.
+    :param seconds: The number of seconds before the cache expires.
+    :param maxsize: The maximum size of the LRU cache.
+    :return: The decorated function with caching.
+    """
 
     def wrapper_cache(func):
         func = lru_cache(maxsize=maxsize)(func)
